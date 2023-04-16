55aac308dd8e675278e3051cb782acf81936788d
autumnchris <autumncbullard@gmail.com>
2023-04-16 11:30:41 -0400

    Add a container around the books

--- src/components/ReadingList.js
+++ src/components/ReadingList.js
@@ -7,7 +7,7 @@ const ReadingList = ({ readingList, viewableReadingList, filterSortFormData, set
   return (
     <React.Fragment>
       <FilterSortForm filterSortFormData={filterSortFormData} setFilterSortFormData={setFilterSortFormData} />
-      {viewableReadingList.length !== 0 ? [...viewableReadingList].map((book) => <Book key={book.id} book={book} readingList={readingList} deleteBook={deleteBook} toggleRead={toggleRead} />) : <InfoMessage messageText="No results match your search specifications." />}
+      {viewableReadingList.length !== 0 ? <div className="reading-list">{[...viewableReadingList].map((book) => <Book key={book.id} book={book} readingList={readingList} deleteBook={deleteBook} toggleRead={toggleRead} />)}</div> : <InfoMessage messageText="No results match your search specifications." />}
     </React.Fragment>
   );
 }
