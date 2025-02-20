8f281c58ac4525020d7ff931748c59c76b5d402b
autumnchris <autumncbullard@gmail.com>
2025-02-19 20:46:02 -0500

    Improve semantic HTML

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,7 @@ import getReadingList from '../utils/getReadingList';
 
 const Sidebar = ({ addBookFormData, toggleFormModal }) => {
   return (
-    <div className="col sidebar">
+    <aside className="col sidebar">
       <div className="button-group">
         <button type="button" className="button add-book-button" onClick={() => toggleFormModal(true, 'add', addBookFormData)}><span className="icon fa-solid fa-plus" aria-hidden="true"></span> Add Book</button>
       </div>
@@ -12,7 +12,7 @@ const Sidebar = ({ addBookFormData, toggleFormModal }) => {
         <div className="stat"><span className="stat-label">Read</span> {getReadingList().filter(book => book.read).length}</div>
         <div className="stat"><span className="stat-label">Unread</span> {getReadingList().filter(book => !book.read).length}</div>
       </div>
-    </div>
+    </aside>
   );
 }
 
